a40ab6e6ff03f8efc21f827344d4185d0aec38f3
Craig Petterson <cspetterson@hey.com>
2024-09-12 15:33:20 +0100

    Add position_score, just did db:migrate:redo

--- db/migrate/20240912132012_create_list_items.rb
+++ db/migrate/20240912132012_create_list_items.rb
@@ -2,6 +2,7 @@ class CreateListItems < ActiveRecord::Migration[7.1]
   def change
     create_table :list_items do |t|
       t.string :name
+      t.float :position_score
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema[7.1].define(version: 2024_09_12_132012) do
   create_table "list_items", force: :cascade do |t|
     t.string "name"
+    t.float "position_score"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
