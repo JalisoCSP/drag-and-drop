8ed439e6cb078292730a0aab7cd4a67ca55dc044
Craig Petterson <cspetterson@hey.com>
2024-09-12 14:32:01 +0100

    Ultra basic line styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :list_items
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # root "list_items#index"
+  root "list_items#index"
+
+  resources :list_items
 end
