d27252ab88f18e391727b2b1437e0d1a3c3fdb9a
Craig Petterson <cspetterson@hey.com>
2024-09-13 11:10:11 +0100

    Add draggable_controller + data tages + dropzone element

---
+++ app/javascript/controllers/draggable_controller.js
@@ -0,0 +1,74 @@
+import { Controller } from "@hotwired/stimulus"
+// import { post } from "@rails/request.js"
+
+export default class extends Controller {
+  static targets = [ "line-item" ]
+
+  #draggedElement
+  #nearestElement
+
+  dragStart(event) {
+    event.target.style.opacity = 0.4
+
+    // set draggedElement
+    this.#draggedElement = event.target
+    event.dataTransfer.setData('text/plain', event.target.dataset.draggableId)
+    event.dataTransfer.setData("text/html", event.target.innerHTML)
+
+    event.dataTransfer.dropEffect = "move"
+  }
+
+  dragEnd(event) {
+    console.log("DRAG END")
+    console.log("Dragged Element:", this.draggedElement)
+
+    event.target.style.opacity = 1
+  }
+
+  dragEnter(event) {
+    console.log("DRAG ENTER")
+    event.target.style.color = "red"
+
+    this.#repositionDropzone(event)
+  }
+
+  dragLeave(event) {
+    console.log("DRAG LEAVE")
+
+    this.#repositionDropzone(event)
+  }
+
+  dragOver(event) {
+    console.log("DRAG OVER")
+
+    if (event.preventDefault) {
+      event.preventDefault();
+    }
+    return false
+  }
+
+  drop(event) {
+    console.log("DROP")
+    const draggedItemId = event.dataTransfer.getData('text/plain')
+    const draggedItemHtml = event.dataTransfer.getData('text/html')
+    console.log("draggedItemId:", draggedItemId)
+    console.log("draggedItemHtml:", draggedItemHtml)
+
+    this.#dropzoneElement.classList.add("hidden")
+  }
+
+  #repositionDropzone(event) {
+    const nearestElement = event.target.closest('[data-target="line-item"]')
+    const targetRect = nearestElement.getBoundingClientRect()
+
+    this.#dropzoneElement.classList.remove("hidden")
+
+    if (event.clientY < targetRect.top) {
+      nearestElement.insertAdjacentElement('beforebegin', this.#dropzoneElement)
+    } else {
+      nearestElement.insertAdjacentElement('afterend', this.#dropzoneElement)
+    }
+  }
+
+  #dropzoneElement = document.getElementById('dropzone')
+}
